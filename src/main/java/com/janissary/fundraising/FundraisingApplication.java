563b11eb1f65dccb46bd2bd111d7f104b137ab02
uranium-boy <janczara.maciej@gmail.com>
2025-05-14 14:13:13 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/janissary/fundraising/FundraisingApplication.java
@@ -0,0 +1,13 @@
+package com.janissary.fundraising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundraisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundraisingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janissary/fundraising/FundraisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janissary.fundraising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
