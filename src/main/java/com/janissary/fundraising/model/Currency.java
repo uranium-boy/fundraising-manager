9044a58b1fde349a2e79579c67797649b6ae9a5d
uranium-boy <janczara.maciej@gmail.com>
2025-05-18 12:11:29 +0200

    refactor(model): add Lombok annotations to Currency and Event entities

--- src/main/java/com/janissary/fundraising/model/Currency.java
+++ src/main/java/com/janissary/fundraising/model/Currency.java
@@ -1,11 +1,19 @@
 package com.janissary.fundraising.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "currencies")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Currency {
     // TODO: maybe use JSR-354?
     @Id
--- src/main/java/com/janissary/fundraising/model/Event.java
+++ src/main/java/com/janissary/fundraising/model/Event.java
@@ -1,25 +1,33 @@
 package com.janissary.fundraising.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Set;
 
 @Entity
 @Table(name = "events")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     @ManyToOne()
-    @JoinColumn(name = "default_currency_id")
+    @JoinColumn(name = "default_currency_id", nullable = false)
     private Currency defaultCurrency;
 
-    @Column(precision = 19, scale = 6)
-    private BigDecimal totalCollected;
+    @Column(precision = 19, scale = 2, nullable = false)
+    @Builder.Default
+    private BigDecimal totalCollected = BigDecimal.ZERO;
 
     @OneToMany(mappedBy = "event")
     private Set<CollectionBox> boxes;
