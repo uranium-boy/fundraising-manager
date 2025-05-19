92b4832ad262539970d9a9eeda89abdf0b4989f5
uranium-boy <janczara.maciej@gmail.com>
2025-05-19 12:39:05 +0200

    feat(controller): wire CollectionBoxController to CollectionBoxServiceImpl

--- src/main/java/com/janissary/fundraising/controller/CollectionBoxController.java
+++ src/main/java/com/janissary/fundraising/controller/CollectionBoxController.java
@@ -1,10 +1,10 @@
 package com.janissary.fundraising.controller;
 
-import com.janissary.fundraising.dto.CollectionBoxDto;
 import com.janissary.fundraising.dto.request.AssignBoxRequest;
 import com.janissary.fundraising.dto.request.DonateMoneyRequest;
-import com.janissary.fundraising.dto.response.CollectionBoxStatus;
+import com.janissary.fundraising.dto.response.CollectionBoxInfo;
 import com.janissary.fundraising.dto.response.CreateCollectionBoxResponse;
+import com.janissary.fundraising.service.impl.CollectionBoxServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +14,23 @@ import java.util.List;
 
 @RestController
 public class CollectionBoxController {
+    private final CollectionBoxServiceImpl service;
+
+    public CollectionBoxController(CollectionBoxServiceImpl service) {
+        this.service = service;
+    }
+
     @PostMapping("/boxes")
     public ResponseEntity<CreateCollectionBoxResponse> createBox() {
-        // TODO: create new box, return newly created box
-        throw new UnsupportedOperationException("TODO");
+        CreateCollectionBoxResponse response = service.createBox();
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/boxes")
-    public ResponseEntity<List<CollectionBoxStatus>> getBoxes() {
+    public ResponseEntity<List<CollectionBoxInfo>> getBoxes() {
         // TODO: return all boxes, don't specify the event, no actual value
-        throw new UnsupportedOperationException("TODO");
+        List<CollectionBoxInfo> boxList = service.getBoxes();
+        return new ResponseEntity<>(boxList, HttpStatus.OK);
     }
 
     @PostMapping("/boxes/{id}/assign")
@@ -31,28 +38,28 @@ public class CollectionBoxController {
             @PathVariable Long id,
             @RequestBody @Valid AssignBoxRequest assignRequest
     ) {
-        // TODO: assign collection box lmao
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        service.assignBoxToEvent(id, assignRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/boxes/{id}/donate")
     public ResponseEntity<Void> donateMoney(
             @PathVariable Long id,
-            @RequestBody @Valid DonateMoneyRequest req
+            @RequestBody @Valid DonateMoneyRequest donateRequest
             ) {
-        // TODO: add money to current amount
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        service.donateMoney(id, donateRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/boxes/{id}/transfer")
     public ResponseEntity<Void> transferMoney(@PathVariable Long id) {
-        // TODO: clear account, convert currencies and add to event's account
-        throw new UnsupportedOperationException("TODO");
+        service.transferMoney(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/boxes/{id}")
     public ResponseEntity<Void> unregisterBox(@PathVariable Long id) {
-        // TODO: don't transfer money, delete box
-        throw new UnsupportedOperationException("TODO");
+        service.unregisterBox(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
